85b51283426f91c9c511697f01b0254c7046ca07
Anupreet Mishra <81185930+AnupreetMishra@users.noreply.github.com>
2022-02-27 23:00:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age:"))
+
+if age<18:
+  print("you are not eligible for driving")
+
+elif age==18:
+  print("We will think about you")
+
+else:
+  print("you are eligible for driving")
